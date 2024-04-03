a7f8088dc40eb1bd37d45a4f96ceafdaf5a41f74
Lucas Lima <lucaserolima@gmail.com>
2024-04-02 21:42:16 -0400

    feat: Create the pagination controller, with the 'getUsers' method

---
+++ pagination-angular/src/main/java/br/com/ero/paginationangular/resource/UserResource.java
@@ -0,0 +1,41 @@
+package br.com.ero.paginationangular.resource;
+
+
+import br.com.ero.paginationangular.domain.HttpResponse;
+import br.com.ero.paginationangular.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.Optional;
+
+import static java.time.LocalDateTime.now;
+
+@RestController
+@RequestMapping("/api/pagination")
+@RequiredArgsConstructor
+public class UserResource {
+
+    private final UserService userService;
+
+
+    @GetMapping("/users")
+    public ResponseEntity<HttpResponse> getUsers(@RequestParam Optional<String> name,
+                                                 @RequestParam Optional<Integer> page,
+                                                 @RequestParam Optional<Integer> size) {
+        return ResponseEntity.ok().body(
+                HttpResponse.builder()
+                        .timeStamp(now().toString())
+                        .data(Map.of("page", userService.getUsers(name.orElse(""), page.orElse(0), size.orElse(10))))
+                        .message("Users Retrieved")
+                        .status(HttpStatus.OK)
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        );
+    }
+}
