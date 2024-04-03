802b9a92bafa3011271cbc7857db2171fc4d212e
Lucas Lima <lucaserolima@gmail.com>
2024-04-02 21:05:32 -0400

    feat: Create Project Backend

---
+++ pagination-angular/src/main/java/br/com/ero/paginationangular/PaginationAngularApplication.java
@@ -0,0 +1,13 @@
+package br.com.ero.paginationangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginationAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginationAngularApplication.class, args);
+	}
+
+}
---
+++ pagination-angular/src/test/java/br/com/ero/paginationangular/PaginationAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ero.paginationangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginationAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
