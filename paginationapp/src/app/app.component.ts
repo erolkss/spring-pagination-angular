35122f14d1137b5ef9ad85cf43a16d4b48857411
Lucas Lima <lucaserolima@gmail.com>
2024-04-03 20:21:42 -0400

    feat: Implemented the application state checker and disabled some tscondig.json properties that prevent the class from being initialized in the constructor

--- paginationapp/src/app/app.component.ts
+++ paginationapp/src/app/app.component.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
+import { UserService } from './service/user.service';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable, catchError, map, of, startWith } from 'rxjs';
+import { ApiResponse } from './interface/api-response';
+import { Page } from './interface/page';
+import { HttpErrorResponse } from '@angular/common/http';
+import { response } from 'express';
+import { error } from 'console';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +15,21 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'paginationapp';
+export class AppComponent implements OnInit {
+  usersState$: Observable<{appState: string, appData?: ApiResponse<Page>, error?: HttpErrorResponse}>;
+
+  constructor (
+    private UserService: UserService
+  ){}
+
+  ngOnInit(): void {
+    this.usersState$ = this.UserService.users$().pipe(
+      map((response: ApiResponse<Page>) => {
+        console.log(response);
+        return ({ appState: 'APP_LOADED', appData: response})
+      }
+    ),
+    startWith({ appState: 'APP_LOADING'}),
+    catchError((error: HttpErrorResponse) => of({ appState: 'APP_ERROR', error}))
+    )}
 }
